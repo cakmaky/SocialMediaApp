7455c88555b383e171a4025c243107cb5457e8bc
Yalcin Cakmak <yc@YCs-MacBook-Pro.local>
2017-01-10 21:49:37 -0800

    Completed firebase integration

---
+++ SocialMediaApp/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+// SignInVC.swift
+//  SocialMediaApp
+//
+//  Created by YC on 1/10/17.
+//  Copyright © 2017 Cakmak LLC. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
