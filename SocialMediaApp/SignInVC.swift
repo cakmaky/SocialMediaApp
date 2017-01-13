4b5dc33e53138b87435259cfbbfa28547deeb41a
Yalcin Cakmak <yc@YCs-MacBook-Pro.local>
2017-01-12 22:04:12 -0800

    successfully finished email authentication

--- SocialMediaApp/SignInVC.swift
+++ SocialMediaApp/SignInVC.swift
@@ -13,6 +13,11 @@ import Firebase
 
 
 class SignInVC: UIViewController {
+    
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var pwdField: FancyField!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,6 +57,27 @@ class SignInVC: UIViewController {
             }
             
         })
+    }
+    @IBAction func signInTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let pwd = pwdField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("ALERT: Email user authenticated with Firebase!")
+                } else {
+                    
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user2, error2) in
+                        if error2 != nil {
+                             print("ALERT:Unable to authentica with Firebase using email")
+                        } else {
+                            print("ALERT:Successfully authenticated with Firebase ")
+                        }
+                    })
+                    
+                }
+            })
+            
+        }
         
         
     }
