2a7ccd910185420313d6e3074a9032cb5a13f942
Yalcin Cakmak <yc@YCs-MacBook-Pro.local>
2017-01-14 12:38:15 -0800

    table view configured

---
+++ SocialMediaApp/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  SocialMediaApp
+//
+//  Created by YC on 1/13/17.
+//  Copyright © 2017 Cakmak LLC. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+    }
+
+    
+    
+    
+    
+}
