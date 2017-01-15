a4f5b46e8edac303db651d03affeda228881b0f8
Yalcin Cakmak <yc@YCs-MacBook-Pro.local>
2017-01-14 18:32:54 -0800

    firebase get completed

--- SocialMediaApp/FeedVC.swift
+++ SocialMediaApp/FeedVC.swift
@@ -14,6 +14,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
     
         super.viewDidLoad()
@@ -22,9 +24,23 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value)
+            
+            self.posts = []
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                        
+                    }
+                }
+                
+                
+            }
+            self.tableView.reloadData()
         })
-
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -32,10 +48,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("ALERT: \(post.caption)")
+        
+        
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
@@ -49,3 +70,12 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
 
 }
+
+
+
+
+
+
+
+
+
